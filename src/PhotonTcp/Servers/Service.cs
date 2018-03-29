0378de9236cb55fbf7271d86cebbb5a130a4413e
Kelly Gendron (Robins) <Github@404htm.com>
2018-03-28 23:30:44 -0400

    Rename of CoreTcp -> PhotonTcp

--- src/PhotonTcp/Servers/Service.cs
+++ src/PhotonTcp/Servers/Service.cs
@@ -13,23 +13,22 @@ namespace Photon
 
     public class Service<T> : Service
     {
-        private IMethodBuilder _methodBuilder;
-        
-        private Func<T> _impl;
+        private readonly IMethodBuilder _methodBuilder;
+        private readonly Func<T> _impl;
         private List<Method> _methods;
 
+        internal Service(T implementation, IMethodBuilder methodBuilder) : this(implementation)
+        {
+            _methodBuilder = methodBuilder;
+        }
         
         public Service(T implementation)
         {
             _impl = () => implementation;
         }
 
-        internal Service(T implementation, IMethodBuilder methodBuilder) : this(implementation)
-        {
-            _methodBuilder = methodBuilder;
-        }
-
         private T GetImpl() => _impl();
+        
         public object[] Call(int method, object[] args) => _methods[method].Run(GetImpl(), args);
 
         public void Compile()
